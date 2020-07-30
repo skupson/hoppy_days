1fa08dca30237f923f2edbb26adca395f142a211
skupson <andrija.skupinski.1.16@ict.edu.rs>
2020-07-30 17:56:22 +0200

    Added the jump ability for the player

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -2,13 +2,23 @@ extends KinematicBody2D
 
 var motion = Vector2(0, 0)
 var speed = 500
-var gravity = 100
+var jump_speed = 500
+var gravity = 10
 
 func _physics_process(delta):
+	apply_gravity()
 	if Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
-		motion.x = 1
+		motion.x = speed
 	elif Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
-		motion.x = -1
+		motion.x = -speed
 	else:
 		motion.x = 0
-	move_and_slide(motion * speed)
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y -= jump_speed
+	move_and_slide(motion, Vector2.UP)
+	
+func apply_gravity():
+	if not is_on_floor():
+		motion.y += gravity
+	else:
+		motion.y = 0
